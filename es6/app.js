6fe6e3816e6e06d04110e08626fe6197eac0d684
MaharTariq <mahartariq499@gmail.com>
2020-07-28 17:51:56 +0500

    many new concepts about var let

---
+++ es6/app.js
@@ -0,0 +1,48 @@
+console.log('Hello ES6');
+
+// {
+//     var abc = "xyz";
+//     console.log(abc);
+// }
+// console.log(abc);
+
+// {
+//     let abc = "xyz";
+//     console.log(abc);
+// }
+// console.log(abc);
+
+// function sum(){
+//     var xyz = 'abc';
+//     console.log(xyz);
+// }
+
+// sum();
+// console.log(xyz);
+
+// function sum(){
+//     let xyz = 'abc';
+//     console.log(xyz);
+// }
+
+// sum();
+// console.log(xyz);
+
+// var greet ="Hello";
+// console.log(greet);
+
+// var greet = "Hi";
+// console.log(greet);
+
+let greet = "Good Morning";
+console.log(greet);
+let greet = "Good Night";
+console.log(greet);
+
+// console.log(greet);
+// var greet = "Hello Man";
+// console.log(greet);
+
+// console.log(greet);
+// let greet = "Hi Man";
+// console.log(greet)
